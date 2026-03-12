7b2340dbe427730bb01a8b9a884fd06492ffd0e0
emiralici54547@gmail.com <emiralici54547@gmail.com>
2026-03-12 11:24:35 +0000

    Created BmwAutohandel; Edited settings; Deleted Form1

--- client_code/BmwAutohandel/__init__.py
+++ client_code/BmwAutohandel/__init__.py
@@ -1,7 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import BmwAutohandelTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class BmwAutohandel(BmwAutohandelTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
