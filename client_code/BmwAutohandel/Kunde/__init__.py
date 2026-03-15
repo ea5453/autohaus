3231bf8ad1fd04a31c72b1c552b80af26f24afd9
emiralici54547@gmail.com <emiralici54547@gmail.com>
2026-03-15 13:56:42 +0000

    Created BmwAutohandel.Kunde and Backend; Edited BmwAutohandel

--- client_code/BmwAutohandel/Kunde/__init__.py
+++ client_code/BmwAutohandel/Kunde/__init__.py
@@ -1,11 +1,12 @@
-from ._anvil_designer import BmwAutohandelTemplate
+from ._anvil_designer import KundeTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class BmwAutohandel(BmwAutohandelTemplate):
+
+class Kunde(KundeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
---
+++ server_code/Backend.py
@@ -0,0 +1,26 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import sqlite3
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+@anvil.server.callable
+def check_login(kid,mid,vorname,nachname):
+  with sqlite3.connect(data_files["autohaus.db"]) as conn:
+    cur = conn.cursor()
+    result = cur.execute(kid,mid,vorname,nachname).fetchall()
+  return result
