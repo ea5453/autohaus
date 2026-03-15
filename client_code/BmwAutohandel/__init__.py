519aaad753005a624948d07d8f6087cfecc8328c
emiralici54547@gmail.com <emiralici54547@gmail.com>
2026-03-15 20:05:58 +0000

    Edited BmwAutohandel

--- client_code/BmwAutohandel/__init__.py
+++ client_code/BmwAutohandel/__init__.py
@@ -13,9 +13,20 @@ class BmwAutohandel(BmwAutohandelTemplate):
     return_value = anvil.server.call('select_Mitarbeiter','SELECT DISTINCT Position FROM Mitarbeiter')
     return_value = [entry[0] for entry in return_value]
     self.drop_down_mitarbeiter.items = return_value
-    # Any code you write here will run before the form opens.
+
+  def check_button_sign_in()
 
   @handle("text_box_id", "pressed_enter")
   def text_box_id_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
+    pass
+
+  @handle("radio_button_Kunde", "clicked")
+  def radio_button_Kunde_clicked(self, **event_args):
+    self.drop_down_mitarbeiter.visible = False
+    pass
+
+  @handle("radio_button_Mitarbeiter", "clicked")
+  def radio_button_Mitarbeiter_clicked(self, **event_args):
+    self.drop_down_mitarbeiter.visible = True
     pass
